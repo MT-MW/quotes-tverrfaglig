c32d35a3f9a74550e5c430573a455d1094f0a17b
MariaMedina <mariawestengen@icloud.com>
2026-01-13 14:56:58 +0100

    fixing navbar

--- routes/defaultRoutes.js
+++ routes/defaultRoutes.js
@@ -3,4 +3,6 @@ const defaultController = require('../controllers/defaultController');
 const middleware = require('../middleware/auth');
 const router = express.Router();
 
-router.get('/', m
+router.get('/', middleware.optionalAuthenticate, defaultController.index);
+
+module.exports = router;
