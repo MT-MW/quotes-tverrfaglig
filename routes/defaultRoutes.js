16ae8df7b495302aaf9a9d4cbca1332c4da3efae
MariaMedina <mariawestengen@icloud.com>
2026-01-13 14:50:57 +0100

    creating the possibility to see username in nav on index

--- routes/defaultRoutes.js
+++ routes/defaultRoutes.js
@@ -3,6 +3,4 @@ const defaultController = require('../controllers/defaultController');
 const middleware = require('../middleware/auth');
 const router = express.Router();
 
-router.get('/', middleware.optionalAuthenticate, defaultController.index);
-
-module.exports = router;
+router.get('/', m
